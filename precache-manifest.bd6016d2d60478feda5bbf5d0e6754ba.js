f0b39a3962e981afe271eca13c56acdae4735383
Lachlan Mckellar <lachlan.person@gmail.com>
2020-07-23 19:15:55 +1000

    Updates

---
+++ precache-manifest.bd6016d2d60478feda5bbf5d0e6754ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55fec691a9c4ceafff3aec77a1424703",
+    "url": "/UserSorter/index.html"
+  },
+  {
+    "revision": "3f39f82714b009c3bc73",
+    "url": "/UserSorter/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "2a190c6293d1170d9d6e",
+    "url": "/UserSorter/static/js/2.53217886.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/UserSorter/static/js/2.53217886.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f39f82714b009c3bc73",
+    "url": "/UserSorter/static/js/main.a15593b3.chunk.js"
+  },
+  {
+    "revision": "448f82053d47634f94a3",
+    "url": "/UserSorter/static/js/runtime-main.1165d484.js"
+  }
+]);
